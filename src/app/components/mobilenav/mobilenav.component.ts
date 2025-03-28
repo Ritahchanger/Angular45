b97485d1bd63b25a53a37768b62db220a605981a
Dennis Peter <peterdennis573@gmail.com>
2025-03-28 22:51:49 +0300

    Added a navbar

---
+++ src/app/components/mobilenav/mobilenav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobilenav',
+  imports: [],
+  templateUrl: './mobilenav.component.html',
+  styleUrl: './mobilenav.component.css'
+})
+export class MobilenavComponent {
+
+}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,13 +2,29 @@ import { Component } from '@angular/core';
 
 import { MatIconModule } from '@angular/material/icon';
 
+import { MobilenavComponent } from '../mobilenav/mobilenav.component';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [ MatIconModule],
+  imports: [ MatIconModule,MobilenavComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  mobileNavbar : Boolean =  false;
+
+  showNavbar = () =>{
+
+    this.mobileNavbar = true;
+
+  }
+
+  hideNavbar = () => {
+
+    this.mobileNavbar = false;
+
+  }
 
 }
